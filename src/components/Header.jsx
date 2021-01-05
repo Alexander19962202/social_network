75cd6f35a341f03958ba5ee6c20df295650f56d4
AlexEvs <evstafiadi.shura@yandex.ru>
2021-01-05 16:51:00 +0300

    12 header, navbar, profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://yt3.ggpht.com/a/AATXAJzn-DRRHOx_J9XtPlDYbNjK3eJSVRlvzgZZCMmUmQ=s900-c-k-c0x00ffffff-no-rj'/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img src='https://cs309821.vk.me/v309821389/8138/taqZMGEXKgo.jpg'/>
+            <div>ava + description</div>
+            <div>
+                New posts:
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
