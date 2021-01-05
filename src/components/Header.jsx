c7a1c805c9087780bd1886f5616c64b0f2798dfc
AlexEvs <evstafiadi.shura@yandex.ru>
2021-01-05 20:26:21 +0300

    14 css-modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={classes.header}>
             <img src='https://yt3.ggpht.com/a/AATXAJzn-DRRHOx_J9XtPlDYbNjK3eJSVRlvzgZZCMmUmQ=s900-c-k-c0x00ffffff-no-rj'/>
         </header>
     );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import classes from './Profile.module.css'
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={classes.profile}>
             <img src='https://cs309821.vk.me/v309821389/8138/taqZMGEXKgo.jpg'/>
             <div>ava + description</div>
             <div>
                 New posts:
-                <div>Post 1</div>
-                <div>Post 2</div>
+                <div className={classes.item}>Post 1</div>
+                <div className={classes.item}>Post 2</div>
             </div>
         </div>
     );
