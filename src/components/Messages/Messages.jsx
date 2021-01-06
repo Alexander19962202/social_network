3ecf04262bae38adc301494755d504eca9e5fc6c
AlexEvs <evstafiadi.shura@yandex.ru>
2021-01-06 17:38:03 +0300

    22 routes of dialogs

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import classes from'./Messages.module.css'
+import {NavLink} from "react-router-dom";
 
 const Messages = () => {
     return (
         <div className={classes.messages}>
             <div className={classes.dialogItems}>
-                <div className={classes.dialog + ' ' + classes.active}>Anya</div>
-                <div className={classes.dialog}>Misha</div>
-                <div className={classes.dialog}>Daniil</div>
-                <div className={classes.dialog}>Sergey</div>
-                <div className={classes.dialog}>Vladimir</div>
-                <div className={classes.dialog}>Pavel</div>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    <NavLink to='/messages/1' activeClassName={classes.active}>Anna</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/messages/2' activeClassName={classes.active}>Dmitriy</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/messages/3' activeClassName={classes.active}>Vladimir</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/messages/4' activeClassName={classes.active}>Andrey</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/messages/5' activeClassName={classes.active}>Anton</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/messages/6' activeClassName={classes.active}>Sergey</NavLink>
+                </div>
             </div>
             <div className={classes.messageItems}>
                 <div className={classes.message}>Hello!!</div>
