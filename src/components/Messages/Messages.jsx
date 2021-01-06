10a1a7e6c98cd917d10bd650ba39ac3a60a8eb5b
AlexEvs <evstafiadi.shura@yandex.ru>
2021-01-06 17:07:53 +0300

    21 verstka Messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import './Messages.module.css'
+import classes from'./Messages.module.css'
 
 const Messages = () => {
     return (
-        <div>
-            Messages
+        <div className={classes.messages}>
+            <div className={classes.dialogItems}>
+                <div className={classes.dialog + ' ' + classes.active}>Anya</div>
+                <div className={classes.dialog}>Misha</div>
+                <div className={classes.dialog}>Daniil</div>
+                <div className={classes.dialog}>Sergey</div>
+                <div className={classes.dialog}>Vladimir</div>
+                <div className={classes.dialog}>Pavel</div>
+            </div>
+            <div className={classes.messageItems}>
+                <div className={classes.message}>Hello!!</div>
+                <div className={classes.message}>Hi!</div>
+                <div className={classes.message}>How are you?</div>
+            </div>
         </div>
     );
 }
