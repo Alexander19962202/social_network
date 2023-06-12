290edbd743deb979269b96ce5d931de5edcb1b27
AlexEvs <evstafiadi.shura@yandex.ru>
2023-06-12 22:02:30 +0300

    App test, web vitals typing

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,32 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SocialNetworkApp from './app';
+import SocialNetworkApp from 'src/app';
 import {unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 import mockAxios from 'jest-mock-axios';
 
-let container: any = null;
-beforeEach(() => {
-  // подготавливаем DOM-элемент, куда будем рендерить
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  // подчищаем после завершения
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-  // cleaning up the mess left behind the previous test
-  mockAxios.reset();
-});
-
-it('renders without crashing', () => {
-  // eslint-disable-next-line testing-library/no-unnecessary-act
-  act(() => {
-    mockAxios.get.mockResolvedValue({ data: { resultCode: 0 }, resultCode: 0 });
-    mockAxios.post.mockResolvedValue({ data: {}, resultCode: 0 });
-
-    ReactDOM.render(<SocialNetworkApp />, container);
+describe('App', () => {
+  let container: HTMLDivElement | null
+  beforeEach(() => {
+    // prepare DOM-element for rendering
+    container = document.createElement("div");
+    document.body.appendChild(container);
   });
-});
+
+  afterEach(() => {
+    // clean after test
+    if (container) {
+      unmountComponentAtNode(container);
+      container.remove();
+      container = null;
+    }
+    // cleaning up the mess left behind the previous test
+    mockAxios.reset();
+  });
+
+  it('renders without crashing', () => {
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    act(() => {
+      mockAxios.get.mockResolvedValue({ data: { resultCode: 0 }, resultCode: 0 });
+      mockAxios.post.mockResolvedValue({ data: {}, resultCode: 0 });
+
+      ReactDOM.render(<SocialNetworkApp />, container);
+    });
+  });
+})
--- src/index.tsx
+++ src/index.tsx
@@ -9,5 +9,4 @@ ReactDOM.render( <SocialNetworkApp/>, document.getElementById('root') );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// @ts-expect-error TS(2554): Expected 1 arguments, but got 0.
 reportWebVitals();
