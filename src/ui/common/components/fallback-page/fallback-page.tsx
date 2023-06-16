6f57fa26f82432a746b3ec801f02f5c628de30b6
AlexEvs <evstafiadi.shura@yandex.ru>
2023-06-16 07:34:11 +0300

    Fallback and NotFound Pages and some refactoring

---
+++ src/ui/common/components/fallback-page/fallback-page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+type Props = {
+  resetError: () => void
+}
+
+const FallbackPage: React.FC<Props> = ({ resetError }) => {
+  return (
+    <>
+      <div>Oooops! An error has occured</div>
+      <Link onClick={() => resetError()} to='/'>Back to main</Link>
+    </>
+  )
+};
+
+export default FallbackPage;
---
+++ src/ui/common/components/not-found-page/not-found-page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div>
+      <label>Ooops!!!</label>
+      <div>404 NOT FOUND</div>
+    </div>
+  )
+};
+
+export default NotFoundPage;
