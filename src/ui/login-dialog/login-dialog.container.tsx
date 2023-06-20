0eff7dc9c56d9900e5be8891acfacad2640156c9
AlexEvs <evstafiadi.shura@yandex.ru>
2023-06-20 07:53:02 +0300

    [Redux Toolkit][Login Dialog Component] Migrating connect to Hooks

--- src/ui/login-dialog/login-dialog.container.tsx
+++ src/ui/login-dialog/login-dialog.container.tsx
@@ -1,33 +1,28 @@
 import React from 'react';
-import { connect, ConnectedProps } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { login } from 'src/store/slices/auth/auth.thunks';
 import { Navigate } from 'react-router-dom';
 import LoginDialog, { LoginData } from 'src/ui/login-dialog/login-dialog';
-import { RootState } from 'src/store/store';
+import { AppDispatch, RootState } from 'src/store/store';
 
-const mapStateToProps = (state: RootState) => ({
-  isAuth: state.auth.authUserData.isAuth,
-  captchaURL: state.auth.authUserData.captchaURL,
-});
+const LoginDialogContainer: React.FC = () => {
+  const isAuth = useSelector((state: RootState) => state.auth.authUserData.isAuth);
+  const captchaURL = useSelector((state: RootState) => state.auth.authUserData.captchaURL);
+  const dispatch = useDispatch<AppDispatch>();
 
-const connector = connect(mapStateToProps, { login });
-
-type Props = ConnectedProps<typeof connector>;
-
-const LoginDialogContainer: React.FC<Props> = props => {
   const onSubmit = (formData: LoginData) => {
-    props.login(formData.email, formData.password, formData?.rememberMe, formData?.captcha);
+    dispatch(login(formData.email, formData.password, formData?.rememberMe, formData?.captcha));
   };
-  if (props.isAuth) {
+  if (isAuth) {
     return <Navigate to={'/profile'} />;
   }
 
   return (
     <div>
       <h1>Login</h1>
-      <LoginDialog captchaURL={props.captchaURL} onSubmit={onSubmit} />
+      <LoginDialog captchaURL={captchaURL} onSubmit={onSubmit} />
     </div>
   );
 };
 
-export default connector(LoginDialogContainer);
+export default LoginDialogContainer;
